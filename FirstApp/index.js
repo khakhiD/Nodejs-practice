6c1e03f2abe60484dce50f39b010355caca8208f
khakhiD <kakkiid@gmail.com>
2023-03-09 22:51:03 +0900

    routing

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to the home page!')
 })
 
+/* 경로 매개변수(Route parameters) 설정하기
+    콜론(:)을 통해 매개변수를 지정할 수 있다.
+    만약 Route path가 /users/:userId/books/:bookId 이고,
+    요청한 URI가 http://localhost:3000/user/34/books/8989이면
+    req.params의 값은 {"userId":"34","bookId":"8989"}가 된다.
+*/
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
