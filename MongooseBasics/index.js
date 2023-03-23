a92758e2514b0fd48230226ec9864d53166bdf85
khakhiD <kakkiid@gmail.com>
2023-03-23 23:34:04 +0900

    eslinting

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -1,23 +1,26 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    })
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/movieApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO ERROR!!!!");
+    console.log(err);
+  });
 
 const movieSchema = new mongoose.Schema({
-    title: String,
-    year: Number,
-    score: Number,
-    rating: String
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
 });
 
-const Movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
-
 
 // Movie.insertMany([
 //     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
@@ -30,9 +33,3 @@ const Movie = mongoose.model('Movie', movieSchema);
 //         console.log("IT WORKED!")
 //         console.log(data);
 //     })
-
-
-
-
-
-
