4357ea3af92b9008511435d49e4848e42452fd47
khakhiD <kakkiid@gmail.com>
2023-04-04 22:36:40 +0900

    Session

---
+++ Session_and_Flash/Session/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+const sessionOptions = {
+    secret: '비밀',
+    resave: false,
+    saveUninitialized: false
+}
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`이 페이지를 ${req.session.count}번 보셨어요.`);
+})
+
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}!`);
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
